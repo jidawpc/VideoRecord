a4d0780e25baf5f44896a1edfa633f4fb79a18f8
wpc <jidawangzai@163.com>
2025-06-09 19:35:06 +0800

    feat: init iOS project

---
+++ VideoRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoRecord
+//
+//  Created by wpc on 9/6/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
